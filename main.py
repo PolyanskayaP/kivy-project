6c00ea3e45f663ffd0ef3973b6e08a3e5c2463f9
Полина Полянская <ppolina353@yandex.ru>
2022-07-18 11:17:07 +0300

    commit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from kivy.app import App 
 from kivy.uix.label import Label
+from kivy.uix.button import Button 
+#если работаем более, чем с одним объектом, то надо помещать внутрь слоя:
+from kivy.uix.boxlayout import BoxLayout 
 
 from kivy.core.window import Window
 
@@ -10,9 +13,13 @@ Window.title = "Приложение"
 class MyApp(App):
 
     def build(self):
+        box = BoxLayout()
+        btn = Button(text='Нажми на меня')        
         label = Label(text='Моя программа\nВсё работает!')
+        box.add_widget(label)
+        box.add_widget(btn)
 
-        return label
+        return box 
 
 if __name__ == "__main__":
     MyApp().run()  
